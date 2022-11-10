fc223c630601a05e6ce62ac3365193cab89af232
Evgeniy Budaev <budaev.e@gmail.com>
2022-11-10 20:16:01 +0300

    Interface JPA Repository

---
+++ src/main/java/com/example/spring_zaur_data_jpa/SpringZaurDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_zaur_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZaurDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZaurDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/spring_zaur_data_jpa/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.spring_zaur_data_jpa.service;
+
+import com.example.spring_zaur_data_jpa.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+
+    public List<Employee> findAllByName(String name);
+}
